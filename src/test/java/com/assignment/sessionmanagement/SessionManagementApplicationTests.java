d28d0c88016bd72c33719667ba6b72acb7099bfe
Sahil Mehta <sahilmehta743@gmail.com>
2020-11-23 13:57:03 +0530

    Initial Commit

---
+++ src/test/java/com/assignment/sessionmanagement/SessionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.sessionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
